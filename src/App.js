6035ae20b6e53cd2aae669887f12a436d2755257
FajarBay <fajarbayu90@gmail.com>
2019-05-18 18:37:14 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/Home";
+import Rara from "./pages/Rara";
+import Daru from "./pages/Daru";
+import Fajar from "./pages/Fajar";
+import Error from "./pages/Error";
+import Navbar from "./pages/Navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Navbar/>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/Rara" component={Rara} />
+          <Route path="/Daru" component={Daru} />
+          <Route path="/Fajar" component={Fajar} />
+          <Route component={Error} />
+        </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className="main-top">
+        <header>
+           <div className="container-fluid px-lg-5 ">
+               <nav className="mnu mx-auto">
+                         <label htmlFor="drop" className="toggle">Anggota</label>
+                           <ul className="menu">
+                               <li className="mr-lg-4 mr-3"><NavLink to="/">Septi</NavLink></li>
+                               <li className="mr-lg-4 mr-3"><NavLink to="/Rara">Rara</NavLink></li>
+                               <li className="mr-lg-4 mr-3"><NavLink to="/Daru">Daru</NavLink></li>
+                               <li className="mr-lg-4 mr-3"><NavLink to="/Fajar">Fajar</NavLink></li>
+                           </ul>
+                </nav>
+            </div>
+            </header>
+       </div>
+    );
+}
+
+export default Navbar;
