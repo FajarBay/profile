5009838b520f1181f6db50c1b9cef80f40ed82e2
FajarBay <fajarbayu90@gmail.com>
2019-05-18 18:41:18 +0700

    Updates

---
+++ precache-manifest.0e86e82c0c69c79a9c0d6b253c292128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613d259e63cf710cf46f1a40026c9b28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "351d88e19580feaf9e0e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "cdc1d07d210e00d88355",
+    "url": "/static/js/2.22ed9d80.chunk.js"
+  },
+  {
+    "revision": "351d88e19580feaf9e0e",
+    "url": "/static/js/main.2051f78f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
